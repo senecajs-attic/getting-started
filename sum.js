7924f35b249618ba35255ec23db6a1a92b24bc97
Richard Rodger <richard@ricebridge.com>
2015-07-02 15:19:17 +0100

    better variable names

--- sum.js
+++ sum.js
@@ -1,13 +1,15 @@
 var seneca = require('seneca')()
 
-seneca.add( {role:'math', cmd:'sum'}, 
-            function( args, done ) {
-              var sum = args.left + args.right
-              done( null, { answer: sum } )
-            })
+seneca.add( 
+  {role:'math', cmd:'sum'}, 
+  function( msg, respond ) {
+    var sum = msg.left + msg.right
+    respond( null, { answer: sum } )
+  })
 
-seneca.act( {role:'math', cmd:'sum', left:1, right:2}, 
-            function(err,result) {
-              if( err ) return console.error( err )
-              console.log(result)
-            })
+seneca.act( 
+  {role:'math', cmd:'sum', left:1, right:2}, 
+  function( err, result ) {
+    if( err ) return console.error( err )
+    console.log( result )
+  })
