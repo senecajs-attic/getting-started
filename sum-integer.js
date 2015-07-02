dce8b41a9c3b3deb270ae2c85608e723852ebba1
Richard Rodger <richard@ricebridge.com>
2015-07-02 16:55:30 +0100

    sum-integer.js

---
+++ sum-integer.js
@@ -0,0 +1,44 @@
+var seneca = require('seneca')()
+
+seneca
+  .add( 
+    {role:'math', cmd:'sum'}, 
+    function( msg, respond ) {
+      var sum = msg.left + msg.right
+      respond( null, { answer: sum } )
+    })
+
+  // both these messages will match role:math,cmd:sum
+
+  .act( 
+    {role:'math', cmd:'sum', left:1.5, right:2.5}, 
+    console.log // prints { answer: 4 }
+  )
+  .act( 
+    {role:'math', cmd:'sum', left:1.5, right:2.5, integer:true}, 
+    console.log // prints { answer: 4 }
+  )
+
+
+  .add( 
+    {role:'math', cmd:'sum', integer:true}, 
+    function( msg, respond ) {
+      var sum = Math.floor(msg.left) + Math.floor(msg.right)
+      respond( null, { answer: sum } )
+    })
+
+  // this still matches role:math,cmd:sum
+  .act( 
+    {role:'math', cmd:'sum', left:1.5, right:2.5}, 
+    console.log // prints { answer: 4 }
+  )
+
+  // BUT this matches role:math,cmd:sum,integer:true
+  // because it's more specific - more properties match
+  .act( 
+    {role:'math', cmd:'sum', left:1.5, right:2.5, integer:true}, 
+    console.log // prints { answer: 3 } !!!
+  )
+
+
+
